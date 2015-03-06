1d1b3a66bf9ecbc1630390e388f0bf0938ba7bce
Chengchen JI <jichengchen1988@gmail.com>
2015-03-06 11:44:32 +0100

    remove footer and change default sidebar

--- wp-content/themes/ascent/footer.php
+++ wp-content/themes/ascent/footer.php
@@ -11,26 +11,11 @@
     </div><!-- close .container -->
 </div><!-- close .main-content -->
 
-<footer id="colophon" class="site-footer" role="contentinfo">
-    <div class="container animated fadeInLeft">
-        <div class="row">
-            <div class="site-footer-inner col-sm-12 clearfix">
-            <?php get_sidebar( 'footer' ); ?>
-            </div>  
-        </div>
-    </div><!-- close .container -->
-    <div id="footer-info">
-        <div class="container">
-            <div class="site-info">
-                <?php do_action( 'ascent_credits' ); ?>
-                <a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'ascent' ); ?>" ><?php printf( __( '&copy; 2014 ascent. All rights reserved', 'ascent' ), 'WordPress' ); ?></a>	
-                <span class="sep"> | </span>
-                <?php printf( __( '%1$s  ', 'ascent' ), 'Ascent by '); ?><a href="<?php echo esc_url( __( 'http://nettantra.com/', 'ascent' ) ); ?>"><?php printf( __( 'NetTantra', 'ascent' ), 'NetTantra' ); ?></a>
-            </div><!-- close .site-info -->
-        </div>
-    </div>
-</footer><!-- close #colophon -->
 <a href="#top" id="scroll-top"></a>
+
+<hr>
+<div class="text-center">Yet this is not the end, it's just a new start.</div>
+<br>
 
 <?php wp_footer(); ?>
 
--- wp-content/themes/ascent/sidebar-footer.php
+++ wp-content/themes/ascent/sidebar-footer.php
@@ -12,31 +12,22 @@
 
       <?php do_action( 'before_sidebar' ); ?>
       <?php if ( ! dynamic_sidebar( 'sidebar-footer' ) ) : ?>
-  
+
         <aside id="pages" class="widget widget_pages">
           <?php wp_list_pages(); ?>
         </aside>
-  
+
         <aside id="search" class="widget widget_search">
           <?php get_search_form(); ?>
         </aside>
-  
+
         <aside id="archives" class="widget widget_archive">
           <h3 class="widget-title"><?php _e( 'Archives', 'ascent' ); ?></h3>
           <ul>
             <?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
           </ul>
         </aside>
-  
-        <aside id="meta" class="widget widget_meta"> 
-          <h3 class="widget-title"><?php _e( 'Meta', 'ascent' ); ?></h3>
-          <ul>
-            <?php wp_register(); ?>
-            <li><?php wp_loginout(); ?></li>
-            <?php wp_meta(); ?>
-          </ul>
-        </aside>
-  
+
       <?php endif; ?>
-      
+
     </div><!-- close .sidebar-padder -->
