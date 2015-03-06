44f7bf62354c5e281d57d30e9e37f9bb3bc3c211
Chengchen JI <jichengchen1988@gmail.com>
2015-03-06 16:13:25 +0100

    get rid of some stuff

--- wp-content/themes/ascent/comments.php
+++ wp-content/themes/ascent/comments.php
@@ -80,17 +80,9 @@ if ( post_password_required() )
 	'cancel_reply_link' => __( 'Cancel Reply', 'ascent' ),  // that's the wordpress default value! delete it or edit it ;)
 	'label_submit'      => __( 'Post Comment', 'ascent' ),  // that's the wordpress default value! delete it or edit it ;)
       
-	'comment_field' =>  '<p><textarea placeholder="Start typing..." id="comment" class="form-control" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>', 
-      
-	'comment_notes_after' => '<p class="form-allowed-tags">' .
-	      __( 'You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes:' ) .
-	      '</p><div class="alert alert-info">' . allowed_tags() . '</div>' 
+	'comment_field' =>  '<p><textarea placeholder="Start typing..." id="comment" class="form-control" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
 
-	// So, that was the needed stuff to have bootstrap basic styles for the form elements and buttons 
-
-	// Basically you can edit everything here! 
-	// Checkout the docs for more: http://codex.wordpress.org/Function_Reference/comment_form
-	// Another note: some classes are added in the bootstrap-wp.js - ckeck from line 1 
-    )); 
+	'comment_notes_after' => ''
+	));
     ?>
 </div><!-- #comments -->
--- wp-content/themes/ascent/includes/template-tags.php
+++ wp-content/themes/ascent/includes/template-tags.php
@@ -31,25 +31,7 @@ function ascent_content_nav( $nav_id ) {
 
     ?>
     <nav role="navigation" id="<?php echo esc_attr( $nav_id ); ?>" class="<?php echo $nav_class; ?>">
-	<h1 class="screen-reader-text"><?php _e( 'Post navigation', 'ascent' ); ?></h1>
-	<ul class="pager">
-
-	<?php if ( is_single() ) : // navigation links for single posts ?>
-
-	    <?php previous_post_link( '<li class="nav-previous previous">%link</li>', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'ascent' ) . '</span> %title' ); ?>
-	    <?php next_post_link( '<li class="nav-next next">%link</li>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'ascent' ) . '</span>' ); ?>
-
-	<?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : // navigation links for home, archive, and search pages ?>
-
-	    <?php if ( get_next_posts_link() ) : ?>
-	    <li class="nav-previous previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'ascent' ) ); ?></li>
-	    <?php endif; ?>
-
-	    <?php if ( get_previous_posts_link() ) : ?>
-	    <li class="nav-next next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'ascent' ) ); ?></li>
-	    <?php endif; ?>
-	<?php endif; ?>
-	</ul>
+	    <h1 class="screen-reader-text"><?php _e( 'Post navigation', 'ascent' ); ?></h1>
     </nav><!-- #<?php echo esc_html( $nav_id ); ?> -->
     <?php
 }
